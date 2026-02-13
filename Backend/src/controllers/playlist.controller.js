5a63188aa080adcefa5913a897d0f8f2114d7e75
Needhiesh Singh <needhieshsingxh@users.noreply.github.com>
2026-02-13 22:48:41 +0530

    feat: enhance create and get user playlists with error handling and validation

--- Backend/src/controllers/playlist.controller.js
+++ Backend/src/controllers/playlist.controller.js
@@ -10,7 +10,7 @@ const createPlaylist = asyncHandler(async (req, res) => {
    
     const {name, description} = req.body
     if(!name || !description){
-        return res.status(400).json(new ApiErro(400, "Name and description required to create a playlist"))
+        return res.status(400).json(new ApiError(400, "Name and description required to create a playlist"))
     } 
 
     const userId = req.user?._id;
@@ -19,21 +19,75 @@ const createPlaylist = asyncHandler(async (req, res) => {
     }
 
     const {videoId} = req.params;
-    if(!videoId){
+    if(!videoId || !isValidObjectId(videoId)){
     return res.status(400).json(400, "Invalid VideoId") }
     
-    
+    const playlist = await Playlist.create(
+       { name, 
+        description,
+        videos: videoId ? [videoId]: [],
+        owner: userId,
+    }
+    )
+
+    if(!playlist){
+        return res.status(500).json(new ApiError(500, "Failed to create playlist"))}
+
+    return res.status(201).json(new ApiResponse(201, playlist, "Playlist created successfully!"))
+
     
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
-    //TODO: get user playlists
+
+    if(!userId){
+        return res.status(400).json(new ApiError(400, "Invalid UserId"));
+    }
+
+    const userPlaylists = await Playlist.aggregate([
+        {
+            $match:{
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        },{
+                    $lookup:{
+                        from: "videos",
+                        localField: "videos",
+                        foreignField: "_id",
+                        as: "videos"
+                    }
+        },
+            {
+            $sort:{createdAt: -1}
+        }
+    ])
+    
+    if(userPlaylists.length === 0 || !userPlaylists){
+        return res.status(400).json(new ApiError(400, "Playlist not found"));
+    }
+
+    return res.status(201).json(new ApiResponse(201, userPlaylists, "User Playlist found successfully"))
+
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     //TODO: get playlist by id
+    if(!playlistId){
+        return res.status(400).json(new ApiError(400, "Invalid playlistId"));
+    }
+
+    const playlistById = Playlist.aggregate([
+     {   $match:{
+            _id: new mongoose.Types.objectId(playlistId)
+        }
+    },
+    {
+
+    }
+    ])
+
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
