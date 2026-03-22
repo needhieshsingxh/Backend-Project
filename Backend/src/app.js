1295354824dc898bf0c52b60dbf13041434f25bf
Needhiesh Singh <needhieshsingxh@users.noreply.github.com>
2026-03-22 16:53:49 +0530

    refactor: clean up app.js and improve formatting; update readme with API routes and details

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,39 +2,39 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-
 const app = express();
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-app.use(express.json({limit: "16kb"}));
-app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-
 //routes import
 
-import userRouter from './routes/user.routes.js';
+import userRouter from "./routes/user.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import healthCheckRouter from "./routes/healthcheck.routes.js";
 import likeRouter from "./routes/like.routes.js";
-import commmentRouter from "./routes/comment.routes.js"
-import subscriptionsRouter from "./routes/subscription.route.js"
-import playlist from "./routes/playlist.routes.js"
-import video from "./routes/video.routes.js"
-import dashboard from "./routes/dashboard.routes.js"
+import commmentRouter from "./routes/comment.routes.js";
+import subscriptionsRouter from "./routes/subscription.route.js";
+import playlist from "./routes/playlist.routes.js";
+import video from "./routes/video.routes.js";
+import dashboard from "./routes/dashboard.routes.js";
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tweet", tweetRouter);
 app.use("/api/v1/", healthCheckRouter);
 app.use("/api/v1/like", likeRouter);
 app.use("/api/v1/comment", commmentRouter);
-app.use("api/v1/subscriptions", subscriptionsRouter);
-app.use("api/v1/playlist", playlist);
-app.use("api/v1/video", video);
-app.use("api/v1/dashboard", dashboard);
+app.use("/api/v1/subscriptions", subscriptionsRouter);
+app.use("/api/v1/playlist", playlist);
+app.use("/api/v1/video", video);
+app.use("/api/v1/dashboard", dashboard);
 
-export {app};   
+export { app };
